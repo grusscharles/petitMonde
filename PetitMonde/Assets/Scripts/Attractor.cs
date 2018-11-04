55a6cbc82624675f666bbf55983a6dc2dae3f7ad
grusscharles <grusscharles@gmail.com>
2018-11-04 20:04:26 +0100

    Test Gravité

---
+++ PetitMonde/Assets/Scripts/Attractor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour {
+    [SerializeField]
+    private Rigidbody ball;
+    [SerializeField]
+    private float attractorCoeff;
+
+    public float attractionRadius;
+
+    private Vector3 ballPos;
+    private Vector3 attractorPos;
+
+	void Start () {
+        attractorPos = this.transform.position;
+	}
+	
+	void FixedUpdate () {
+
+        ballPos = ball.transform.position;
+        Vector3 direction = attractorPos - ballPos;
+        float distanceToBall = Vector3.Magnitude(direction);
+        Vector3 attractorForce = (attractorCoeff / Mathf.Pow(distanceToBall, 2)) * Vector3.Normalize(direction);
+        //Debug.Log("Attractor : " + this.name + "Drection : "+ direction);
+        //Debug.Log("Attractor : " + this.name + "Distance to ball : " + distanceToBall);
+        if (distanceToBall<= 5f* attractionRadius)
+        {
+            ball.AddForce(attractorForce);
+        }
+
+	}
+}
---
+++ PetitMonde/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject attractor;
+    [SerializeField]
+    private GameObject player;
+
+    private float distanceToCenter;
+    
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        distanceToCenter = transform.position.magnitude;
+    }
+    private void Update()
+    {
+        transform.position = player.transform.position + offset;
+        float posMag = transform.position.magnitude;
+        transform.position = (distanceToCenter / posMag) * transform.position;
+
+        transform.LookAt(player.transform);
+      
+    }
+}
+
