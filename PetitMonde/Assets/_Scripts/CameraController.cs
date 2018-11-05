f6791b3ca617af7f87cac9d48160b7a358bcb7a4
grusscharles <grusscharles@gmail.com>
2018-11-05 17:32:03 +0100

    cameraFix

--- PetitMonde/Assets/_Scripts/CameraController.cs
+++ PetitMonde/Assets/_Scripts/CameraController.cs
@@ -4,17 +4,13 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField]
     private float rotationSpeed;
+    [SerializeField]
     private GameObject player;
 
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
     private void Update()
     {
         Vector3 playerPos = player.transform.position;
         Vector3 playerUp = player.transform.up;
-
         transform.RotateAround(playerPos, playerUp, Time.deltaTime * Input.GetAxisRaw("Camera") * rotationSpeed);
     }
 }
