cb52f60155526648daa4d69e1223ec5d858f11f3
grusscharles <grusscharles@gmail.com>
2018-11-05 17:57:46 +0100

    Spawn points wip

---
+++ PetitMonde/Assets/_Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    private List<Transform> spawnPointsList;
+
+	// Use this for initialization
+	void Start () {
+        spawnPointsList = new List<Transform>();
+        //Initialize List
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            spawnPointsList.Add(transform.GetChild(i));
+        }
+
+        SpawnVictory();
+        SpawnPlayer();
+
+    }
+
+    private void SpawnVictory()
+    {
+        int spawnIndex = Random.Range(1,7);
+
+        GameObject victorySpawnPawn = transform.
+    }
+
+    private void SpawnPlayer()
+    {
+        Debug.Log("hey");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ PetitMonde/Assets/_Scripts/Victory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            WinGame();
+        }
+    }
+
+    void WinGame()
+    {
+        Debug.Log("You Win !");
+        Time.timeScale = 0f;
+    }
+}
