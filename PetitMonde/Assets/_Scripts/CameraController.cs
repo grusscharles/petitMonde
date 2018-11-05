699414fd5e0eed71c20c998440d20f937eebe950
grusscharles <grusscharles@gmail.com>
2018-11-05 11:56:58 +0100

    CameraController

--- PetitMonde/Assets/_Scripts/CameraController.cs
+++ PetitMonde/Assets/_Scripts/CameraController.cs
@@ -3,43 +3,19 @@ using System.Collections;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject attractor;
-    [SerializeField]
+    private float rotationSpeed;
     private GameObject player;
-
-    private float distanceToCenter;
-    
-    private Vector3 offset;
-    private Vector3 initialAngles;
 
     void Start()
     {
-        offset = transform.position - player.transform.position;
-        distanceToCenter = transform.position.magnitude;
-        initialAngles = transform.eulerAngles;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
     private void Update()
     {
-        //follow player
-        transform.position = player.transform.position + offset;
-        
-        //stay on radius
-        float posMag = transform.position.magnitude;
-        transform.position = (distanceToCenter / posMag) * transform.position;
+        Vector3 playerPos = player.transform.position;
+        Vector3 playerUp = player.transform.up;
 
-        transform.LookAt(player.transform);
-
-        /*
-        float tetaCam = Mathf.Acos(transform.position.x / transform.position.magnitude);
-        float tetaPlayer = Mathf.Acos(player.transform.position.x / player.transform.position.magnitude);
-        float camAngle = 0.5f * (Mathf.PI - tetaPlayer + tetaCam);
-
-        Debug.Log("Euler Angles : " + transform.eulerAngles);
-        transform.eulerAngles = new Vector3(camAngle, initialAngles.y, initialAngles.z);
-        // the second argument, upwards, defaults to Vector3.up
-        /*Quaternion rotation = Quaternion.LookRotation(-offset, relativeUp);
-        transform.rotation.SetLookRotation(-offset,relativeUp);
-        Debug.Log("relativeUp : " + relativeUp);*/
+        transform.RotateAround(playerPos, playerUp, Time.deltaTime * Input.GetAxisRaw("Camera") * rotationSpeed);
     }
 }
 
