1591cf4b736f755bc5f2dac64488b01e4bae8bba
grusscharles <grusscharles@gmail.com>
2018-11-05 12:19:00 +0100

    Jump

--- PetitMonde/Assets/_Scripts/PlayerController/PlayerMovementScript.cs
+++ PetitMonde/Assets/_Scripts/PlayerController/PlayerMovementScript.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour {
-
-    public float moveSpeed;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
 
     private Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
         moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+     
     }
 
     void FixedUpdate()
     {
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+        //GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+        rb.MovePosition(rb.position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
     }
 }
