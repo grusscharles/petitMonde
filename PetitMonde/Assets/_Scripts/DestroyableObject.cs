69fe247215c9fc27ea503cd24c70ddc75ddb1dfc
grusscharles <grusscharles@gmail.com>
2018-11-05 16:02:50 +0100

    destroy waste

--- PetitMonde/Assets/_Scripts/DestroyableObject.cs
+++ PetitMonde/Assets/_Scripts/DestroyableObject.cs
@@ -6,41 +6,22 @@ public class DestroyableObject : MonoBehaviour {
     [SerializeField]
     private GameObject destroyedPrefab;
 
-    private float timeSinceDestruction =0f;
+    private TimeManager timeManager;
 
-    private GameObject destroyedObject;
 	// Use this for initialization
 	void Start () {
-        destroyedObject = null;
+        timeManager = GameObject.FindObjectOfType<TimeManager>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            Debug.Log("collision");
-            destroyedObject = Instantiate(destroyedPrefab, gameObject.transform.position, gameObject.transform.rotation);
+            timeManager.timeSinceLastDestruction = 0f;
+            timeManager.checkWaste = true;
+            GameObject destroyedObject = Instantiate(destroyedPrefab, gameObject.transform.position, gameObject.transform.rotation);
             destroyedObject.transform.localScale = gameObject.transform.localScale;
             Destroy(gameObject);
-
-            timeSinceDestruction = 0f;
-        }
-    }
-
-    private void Update()
-    {
-        timeSinceDestruction += Time.deltaTime;
-
-        if(timeSinceDestruction > 2f)
-        {
-            if(destroyedObject!= null)
-            {
-                Destroy(destroyedObject);
-            }
-            else
-            {
-                Debug.LogWarning("destroyed object is null");
-            }
         }
     }
 
---
+++ PetitMonde/Assets/_Scripts/TimeManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+    private float timer;
+    [HideInInspector]
+    public float timeSinceLastDestruction;
+    [HideInInspector]
+    public GameObject[] wasteArray;
+    [HideInInspector]
+    public bool checkWaste = false;
+
+    // Use this for initialization
+    void Start () {
+        wasteArray = null;
+        timer = 0f;
+        timeSinceLastDestruction = 0f;
+    }
+	
+	void Update () {
+        timer += Time.deltaTime;
+        timeSinceLastDestruction += Time.deltaTime;
+        //Destroy Waste
+        if (checkWaste)
+        {
+            DestroyWaste();
+        }
+    }
+
+    private void DestroyWaste()
+    {
+        wasteArray = GameObject.FindGameObjectsWithTag("waste");
+
+        if (timeSinceLastDestruction > 2f && wasteArray.Length != 0)
+        {
+            foreach (GameObject waste in wasteArray)
+            {
+                Destroy(waste);
+                checkWaste = false;
+            }
+        }
+    }
+}
