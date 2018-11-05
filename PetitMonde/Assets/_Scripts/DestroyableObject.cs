5f6a497fcc2b2cbe09b58af67dfb86c172e770be
grusscharles <grusscharles@gmail.com>
2018-11-05 15:06:46 +0100

    Finalisation prefabs batiments

--- PetitMonde/Assets/_Scripts/DestroyableObject.cs
+++ PetitMonde/Assets/_Scripts/DestroyableObject.cs
@@ -6,20 +6,42 @@ public class DestroyableObject : MonoBehaviour {
     [SerializeField]
     private GameObject destroyedPrefab;
 
+    private float timeSinceDestruction =0f;
+
+    private GameObject destroyedObject;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        destroyedObject = null;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             Debug.Log("collision");
-            GameObject destroyedObject = Instantiate(destroyedPrefab, gameObject.transform.position, gameObject.transform.rotation);
+            destroyedObject = Instantiate(destroyedPrefab, gameObject.transform.position, gameObject.transform.rotation);
             destroyedObject.transform.localScale = gameObject.transform.localScale;
             Destroy(gameObject);
+
+            timeSinceDestruction = 0f;
         }
-      
     }
+
+    private void Update()
+    {
+        timeSinceDestruction += Time.deltaTime;
+
+        if(timeSinceDestruction > 2f)
+        {
+            if(destroyedObject!= null)
+            {
+                Destroy(destroyedObject);
+            }
+            else
+            {
+                Debug.LogWarning("destroyed object is null");
+            }
+        }
+    }
+
 }
